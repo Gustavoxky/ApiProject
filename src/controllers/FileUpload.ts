44bfa49173c3e756b2884fe7124661016881e4c1
Gustavoxky <gustavoxky@gmail.com>
2023-06-07 16:21:39 -0300

    update

--- src/controllers/FileUpload.ts
+++ src/controllers/FileUpload.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express";
 import prisma from "../database/PrismaClient";
 
-
 export class FileUpload {
   async store(req: Request, res: Response) {
     const requestFiles = req.files as Express.Multer.File[];
+
+    if (!requestFiles || requestFiles.length === 0) {
+      return res.status(400).json({ error: "formato de arquivo não suportado." });
+    }
 
     const files = requestFiles.map((file: Express.Multer.File) => {
       return {
